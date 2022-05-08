74c7e3fd86517a1e450e56b2db7289008aacd427
AxelGamma <axelhgbryan@gmail.com>
2022-05-08 14:37:49 -0500

    update

--- src/Simulacion.java
+++ src/Simulacion.java
@@ -54,14 +54,15 @@ public class Simulacion {
 
     }
 
+    // llamamos a los metodos que vamos a usar
     public void run() {
         llegadas();
         inspeccion();
-        tiempoEspInsp();
+        // tiempoEspInsp();
         descompostura();
         reparación();
 
-        impresionSimulacion();
+        // impresionSimulacion();
         impresionReparacion();
         tiempoEnSistema();
         impresionTimeSistema();
@@ -74,14 +75,17 @@ public class Simulacion {
     // Llegadas con una exponencial de 2 horas
     private void llegadas() {
         int i = 0;
-
+        // realizamos las operaciones
         while (i < ri1.length) {
+            // las guardamos en un arreglo para los tiempo
             xllegadas[i] = -2 * Math.log(ri1[i]);
 
             // Vamos sumando el anterior
             if (i == 0) {
+                // La primera ves pasa directamente
                 horaLlegadas[i] = xllegadas[i];
             } else {
+                // La segunda vez vamos sumando la anterior
                 horaLlegadas[i] = horaLlegadas[i - 1] + xllegadas[i];
             }
             i++;
@@ -108,25 +112,55 @@ public class Simulacion {
     private void inspeccion() {
 
         // Calculo de tiempo de inspeccion
-        // ri2 son nuestro nuevos numeros aleatorios
-
+        // ri2 son nuestro nuevos numeros aleatorios diferentes a los ri1
+        // Inicializamos la primera hora de llegada con la primera hora de entrada
         entradas[0] = horaLlegadas[0];
         int i = 0;
         while (i < horaLlegadas.length) {
+            // El bucle repite hasta terminar con todas las llegadas
             if (i == 0) {
+                // Calculamos el tiempo de inspeccion
                 tInsp[i] = 0.25 + (1.05 - 0.25) * ri2[i];
+                // Luego sumamos la hora de entrada a inspeecion y la hora de entrada del
+                // autobus
                 salidaInsp[i] = entradas[i] + tInsp[i];
-                // Calculamos la variable aleatoria
 
             } else {
+                /*
+                 * Para el primer caso vemos si hora de llegada es mayor o igual a la hora
+                 * ultima
+                 * hora de salida
+                 */
                 if (horaLlegadas[i] >= salidaInsp[i - 1]) {
+                    /* Calculamos el tiempo en la inspeccion */
                     tInsp[i] = 0.25 + (1.05 - 0.25) * ri2[i];
+                    // asignamos ala hora de entrada la hora de llegada del autobus
+                    // esto en caso de que no haya tenido que esperar el autobus
+                    tiempoEspInsp[i] = 0;// el tiempo de espera es cero
                     entradas[i] = horaLlegadas[i];
+                    // calculamos el tiempo de salida sumando la de entrada y el tiempo de
+                    // inspeccion
                     salidaInsp[i] = entradas[i] + tInsp[i];
 
                 } else {
+                    // calculamos el tiempo de inspeccion
                     tInsp[i] = 0.25 + (1.05 - 0.25) * ri2[i];
+                    /*
+                     * calculamos el tiempo de espera en este caso ya que el tiempo de salida de el
+                     * anterior autobus es mayor entonces el autobus actual entra cuando salda el
+                     * que esta
+                     * en la inspeccion
+                     */
+
+                    /*
+                     * asignamos a nuestro arreglo el tiempo de espera= salida del autobus en
+                     * estacion- hora de llegada de autobus
+                     */
+                    tiempoEspInsp[i] = salidaInsp[i - 1] - horaLlegadas[i];
+                    /* Hora de entrada saria la hora de salida del anterior autobus */
                     entradas[i] = salidaInsp[i - 1];
+                    // calculamos el tiempo de salida sumando la de entrada y el tiempo de
+                    // inspeccion
                     salidaInsp[i] = entradas[i] + tInsp[i];
 
                 }
@@ -135,38 +169,29 @@ public class Simulacion {
             i++;
         }
 
-    }
-
-    private void tiempoEspInsp() {
-        int i = 0;
-        while (i < ri1.length) {
-            if (i != 0) {
-                if (salidaInsp[i - 1] < horaLlegadas[i]) {
-                    tiempoEspInsp[i] = 0;
-                } else {
-                    tiempoEspInsp[i] = salidaInsp[i - 1] - horaLlegadas[i];
-                }
-            } else {
-                tiempoEspInsp[i] = 0;
-            }
-
-            i++;
-        }
     }
 
     private void descompostura() {
-        // reparación prob
-        // si 0.3 0.3 0-0.3
+        // Las probabilidades que un autobus este descompuesto son las siguientes
+        // si 0.0 0.3 [0-0.3]
         // no 0.7 1 0.3-1
 
         int i = 0, a = 0;
+        // ciclo que va recorrer todos nuestros numeros aleatorios
         while (i < ri3.length) {
+            /*
+             * En el caso que nuestro numero aleatorio este en el rango
+             * [0.0,0.3] si necesita reparacion
+             */
             if (ri3[i] >= 0.0 && ri3[i] <= 0.3) {
                 descompostura[i] = "Si";
+                // Guardamos el numero del autobus que necesito reparacion
                 autobusesRep[a] = i + 1;
+                // Guardamos la hora de salida de los autobuses descompuestos
                 autobusDescompuesto[a] = salidaInsp[i];
                 a++;
-            } else if (ri3[i] >= 0.3 && ri3[i] <= 1) {
+            } /* En caso contrario no */
+            else if (ri3[i] >= 0.3 && ri3[i] <= 1) {
                 descompostura[i] = "No";
             }
             i++;
@@ -177,24 +202,52 @@ public class Simulacion {
     private void reparación() {
 
         int i = 0;
+        // Recorremos los estatus de los autobuses que necesitaron reparacion
         while (i < descompostura.length) {
             if (autobusDescompuesto[i] != 0) {
+                // En el caso de que sea diferente de cero se hacen las operaciones
                 if (i > 0) {
                     // Entra a reparacion
-
+                    // Asignamos la hora de entrada del auto descompuesto a la hora de entrada a
+                    // reparacion
                     horaEntradaRep[i] = autobusDescompuesto[i];
+                    // vemos si el a<b, osea si el tiempo del auto que entro en al estacion a es
+                    // menor que la b
+                    // Ó si A< a la hora de entrada del siguiente auto
                     if (A < B || A < horaEntradaRep[i]) {
+                        /*
+                         * En el caso de que esten ocupadas las dos estaciones, pero el tiempo de
+                         * la estacion a es menor que la b se pone en cola el siquiente auto y
+                         * calculamos el tiempo de espera
+                         */
                         if (A > horaEntradaRep[i]) {
+                            // Calculamos el tiempo de espera
                             tiempoEspReparacion[i] = Math.abs(A - horaEntradaRep[i]);
+                            // La hora de entrada va ser igual ala salida del auto en la inspeccion
                             horaEntradaRep[i] = A;
+                            // Realizamos las operaciones
                             operacionReparacion(i);
-                        } else {
+                        } /*
+                           * En caso de que no sea la primera condicion pasa directamente
+                           * ya que estaria desocupada la estacion
+                           */
+                        else {
+                            /* Realizamos las operaciones */
                             operacionReparacion(i);
+                            /* A toma el tiempo de salida del acarro actual */
                             A = horaSalidaRep[i];
                         }
+                        // Guardamos en el arreglo que estacion se uso, en este caso fue la 1
                         usoEstacion[i] = 1;
+                        // Y un contador para sacar la estadistica de uso
                         contA++;
-                    } else if (B < A || B < horaEntradaRep[i]) {
+                    } // Esta condicion es igual a la primera solo que estamos tratando con la
+                      // estacion B
+                    else if (B < A || B < horaEntradaRep[i]) {
+                        /*
+                         * las operaicones y comentarios son las misma que en la primera condicion,
+                         * solo que aqui trabajamos con la estacion B
+                         */
                         if (B > horaEntradaRep[i]) {
                             tiempoEspReparacion[i] = Math.abs(A - horaEntradaRep[i]);
                             horaEntradaRep[i] = A;
@@ -204,7 +257,9 @@ public class Simulacion {
                             operacionReparacion(i);
                             B = horaSalidaRep[i];
                         }
+                        // Guardamos en el arreglo que estacion se uso, en este caso fue la 2
                         usoEstacion[i] = 2;
+                        // Y un contador para sacar la estadistica de uso
                         contB++;
                     }
                 } else {
@@ -212,10 +267,12 @@ public class Simulacion {
                     operacionReparacion(i);
                     A = horaSalidaRep[i];
                     usoEstacion[i] = 1;
+                    contA++;
                 }
                 i++;
                 cont++;
-            } else {
+            } // En caso contrario rompemos el bucle
+            else {
                 break;
             }
         }
@@ -234,10 +291,14 @@ public class Simulacion {
         int i = 0, a = 0;
 
         while (i < ri1.length) {
+            // Si el autobus tiene compostura
             if (descompostura[i] == "Si") {
+                // Calculamos el tiempo total en el sistema sumando las siguientes tiempos
+                // Tiempo de espera inspeccion+ Tiempo de inpeccion + Tiempo en reparacion
                 timepoTotalSistema[i] = tiempoEspInsp[i] + tInsp[i] + tiempoReparacion[a];
                 a++;
-            } else {
+            } else {//En cambio de no haber tenido reparacion
+                //Solo calculamos el tiempo de espera en inspeccion y tiempo de la inpeccion
                 timepoTotalSistema[i] = tiempoEspInsp[i] + tInsp[i];
             }
             i++;
@@ -313,10 +374,10 @@ public class Simulacion {
             i++;
         }
 
-        double porcentaje = (double) ((contA + 1) / cont);
+        double porcentaje = (double) ((contA) / cont);
         System.out.printf("%s", "Uso de la estacion A es: % " + porcentaje);
 
-        porcentaje = (double) ((contB + 1) / cont);
+        porcentaje = (double) ((contB) / cont);
         System.out.printf("\n%s", "Uso de la estacion B es: % " + porcentaje);
 
     }
