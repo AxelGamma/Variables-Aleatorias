03b469d9893bdd5416e40f624f6c2696929e8653
AxelGamma <axelhgbryan@gmail.com>
2022-05-08 13:52:03 -0500

    udpate

--- src/Aplicacion.java
+++ src/Aplicacion.java
@@ -5,7 +5,6 @@ public class Aplicacion {
         //Introduccion de los datos a generar
         //Simulacion con 150 autobuses urbanos
         Datos datos = new Datos(150, 150, 150, 75, 150, 20);
-        //ii
         datos.run();
         // VariablesAleatorias aplicacion = new VariablesAleatorias(datos.getPruebas1(), datos.getPruebas2(), datos.getPruebas3(),
         //         datos.getPruebas4(), datos.getPruebas5(),datos.getPruebas6());
