66c4c40bf6e3bd54265d04052ac86d201b79e8ce
AxelGamma <axelhgbryan@gmail.com>
2022-05-17 02:18:47 -0500

    update

--- src/Simulacion/Datos.java
+++ src/Simulacion/Datos.java
@@ -27,11 +27,75 @@ public class Datos {
         // Datos para generar los numeros con las convinaciones ya dadas
         // generamos 256 numeros con las siguientes convinaciones
 
-
-        g1.datos(41, 1203, 16);
+        //Replica 1
+        
+        /*g1.datos(41, 1203, 16);
         g2.datos(295, 419, 16);
         g3.datos(43, 1003, 16);
-        g4.datos(121, 653, 16);
+        g4.datos(121, 653, 16);*/
+
+        //Replica 2
+
+        /*g1.datos(223, 733, 16);
+        g2.datos(377, 611, 16);
+        g3.datos(541, 1013, 16);
+        g4.datos(357, 283, 16);*/
+
+        //Replica3
+
+        /* g1.datos(295, 339, 16);
+        g2.datos(129, 779, 16);
+        g3.datos(71, 709, 16);
+        g4.datos(237, 477, 16); */
+
+        //Replica 4
+
+        /* g1.datos(105, 117, 16);
+        g2.datos(193, 237, 16);
+        g3.datos(341, 2803, 16);
+        g4.datos(467, 245, 16); */
+
+        //Replica 5
+
+        /* g1.datos(533, 6803, 16);
+        g2.datos(685, 5997, 16);
+        g3.datos(717, 8043, 16);
+        g4.datos(777, 20003, 16); */
+        
+        //Replica 6
+        
+        /* g1.datos(697, 14157, 16);
+        g2.datos(839, 15187, 16);
+        g3.datos(35, 13941, 16);
+        g4.datos(509, 1109, 16); */
+        
+        //Replica 7
+        
+        /* g1.datos(469, 9843, 16);
+        g2.datos(17, 24981, 16);
+        g3.datos(679, 25227, 16);
+        g4.datos(111, 21349, 16); */
+
+        //Replica 8
+
+        /* g1.datos(191, 7101, 16);
+        g2.datos(245, 11515, 16);
+        g3.datos(749, 31563, 16);
+        g4.datos(291, 9035, 16); */
+
+        //Replica 9
+
+        /* g1.datos(823, 18723, 16);
+        g2.datos(195, 27603, 16);
+        g3.datos(993, 6387, 16);
+        g4.datos(321, 10437, 16); */
+
+        //Replica 10
+        /* g1.datos(361, 13421, 16);
+        g2.datos(487, 19827, 16);
+        g3.datos(635, 7467, 16);
+        g4.datos(535, 8755, 16); */
+
 
 
         asignacionArreglos(g1.getNumGenerados(), g2.getNumGenerados(), g3.getNumGenerados(), g4.getNumGenerados());
